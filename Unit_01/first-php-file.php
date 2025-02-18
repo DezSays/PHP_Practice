2da64771febe005e013eed04a617fcd564f002aa
Dezarea Bryan <DezareaBryan@gmail.com>
2025-02-18 09:48:07 -0500

    Section 1 Concluded. Installed PHP and MySQL. Created initial PHP file and displayed on local server.

---
+++ Unit_01/first-php-file.php
@@ -0,0 +1,10 @@
+<?php // This will initiate the php block of code
+
+echo 'Howdy yall!'; // This will display `Howdy yall` on the screen 
+// php -S localhost:#### 
+// php -S starts a local development server, the number after local host is the port it will run on
+
+// If the server is running on port 8000, access this page in the browser using: http://localhost:8000/first-php-file.php
+
+// The below tag concludes a php block of code
+?>
