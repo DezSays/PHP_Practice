1e69bb46c99d3389fad9fe566aa1d5436b7fbac7
Dezarea Bryan <DezareaBryan@gmail.com>
2025-02-28 08:39:00 -0500

    Using include so that other pages are displayed. How to incorporate a variable.

---
+++ Unit_02/include/other.php
@@ -0,0 +1,3 @@
+<h1>I am the other.php file</h1>
+
+<!-- It is best practice that all files included end in .php, but this is an example of how we can load php into documents regardless of type. -->
