af1abc39bb8f8036348fe420e83eeff340ef0f5e
Dezarea Bryan <DezareaBryan@gmail.com>
2026-01-22 16:59:43 -0500

    Introducing arrays.

--- Unit_04/arrays_introduced.php
+++ Unit_04/arrays_introduced.php
@@ -6,7 +6,21 @@
                 $categories_classic = array('Programming', 'Business', 'Art History', 'Performance Art', 'Concatenate' . '!!!');
                 $categories_modern  = ['Programming', 'Business', 'Art History', 'Performance Art', 'Concatenate' . '!!!'];
 
+                // Check and display if array is empty
+                var_dump(empty($categories_modern[99]));
 
+                // See if exists by key
+                var_dump(isset($categories_modern[99]));
+
+                // See if specified value exists in category
+                var_dump(in_array('Programming', $categories_classic));
+
+                if (in_array('Programming', $categories_modern)) {
+                    echo "Programming is in our curriculum.\n";
+                }
+
+                // Count how many elements are in an array
+                var_dump(count($categories_modern));
 
             ?>
         </pre>
