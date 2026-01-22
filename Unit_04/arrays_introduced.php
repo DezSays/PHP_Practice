25a884259d35a527fabfba14a5b3945e988018b8
Dezarea Bryan <DezareaBryan@gmail.com>
2026-01-22 16:45:27 -0500

    Introducing arrays.

---
+++ Unit_04/arrays_introduced.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <pre>
+            <?php  
+                // Creating an array assigned to variable
+                $categories_classic = array('Programming', 'Business', 'Art History', 'Performance Art', 'Concatenate' . '!!!');
+                $categories_modern  = ['Programming', 'Business', 'Art History', 'Performance Art', 'Concatenate' . '!!!'];
+
+
+
+            ?>
+        </pre>
+    </body>
+</html>
